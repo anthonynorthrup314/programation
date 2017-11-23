ddacc3bd79d4e232ef7e211121244a7836ac1b41
Anthony Northrup <anthonynorthrup314@gmail.com>
2017-11-22 22:06:54 -0500

    Removed test2.py
    Didn't want to add this anyway

--- test2.py
+++
@@ -1,8 +0,0 @@
-import math
-from transform import *
-
-w, h = 640, 480
-
-print Transform.IDENTITY()
-print Transform.IDENTITY().shift(w / 4, 0)
-print Transform.IDENTITY().shift(w / 4, 0).rotate_about(w / 2, h / 2, math.pi)
